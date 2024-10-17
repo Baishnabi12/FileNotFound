549dfba09c541d1b1e87041e35f4ee12eaeea2a4
baish <baish@BAISHNABI>
2024-10-17 13:29:54 +0530

    Add project files.

---
+++ FileNotFound/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ExceptionAssignment
+{
+    internal class ExceptionProgram4
+    {
+
+        class Program
+        {
+            static void Main()
+            {
+                string filePath = "file.txt";
+                try
+                {
+                    string content = File.ReadAllText(filePath);
+                    Console.WriteLine(content);
+                }
+                catch (FileNotFoundException ex)
+                {
+                    Console.WriteLine(" The file was not found.");
+                }
+            }
+        }
+
+    }
+}
